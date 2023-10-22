97b786e2b39cdaf9052da3162d06a72c9ccbfce9
michu04 <148712379+michu04@users.noreply.github.com>
2023-10-22 19:25:55 +0200

    polynomian.h

---
+++ polynomian.h
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int st (vector<double> w);
+vector<double> add (vector<double> a, vector<double> b);
+vector<double> substract (vector<double> a, vector<double> b);
+vector<double> multiply (vector<double> a, vector<double> b);
